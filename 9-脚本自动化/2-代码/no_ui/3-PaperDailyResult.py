5267b286adfb2cac3cd96238bab1323533c68567
huangning <hun1982@qq.com>
2021-07-18 23:51:49 +0800

    202107182351committed

---
+++ 9-脚本自动化/2-代码/no_ui/3-PaperDailyResult.py
@@ -0,0 +1,174 @@
+from collections import defaultdict
+from datetime import date, datetime
+from pathlib import Path
+from openpyxl.utils import get_column_letter
+from dataclasses import dataclass
+
+from functools import partial
+
+import openpyxl
+import numpy as np
+from pandas import DataFrame
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from vnpy.trader.constant import (Direction, Exchange, Interval, Offset)
+
+@dataclass
+class TradeData:
+    """
+    Trade data contains information of a fill of an order. One order
+    can have several trade fills.
+    """
+
+    symbol: str
+    orderid: str
+    tradeid: str
+    direction: Direction = None
+
+    offset: Offset = Offset.NONE
+    price: float = 0
+    volume: float = 0
+    datetime: datetime = None
+
+
+class PnlCaculate:
+    """"""
+
+    def __init__(self, real_strategy_name: str):
+        """"""
+        self.vt_symbol = ""
+        self.symbol = ""
+        self.exchange = None
+        self.start = None
+        self.end = None
+        self.rate = 0
+        self.slippage = 0
+        self.size = 1
+        self.pricetick = 0
+        self.capital = 1_000_000
+        self.risk_free: float = 0.02
+
+        self.strategy_class = None
+        self.strategy = None
+        self.datetime = None
+
+        self.interval = None
+        self.days = 0
+        self.callback = None
+        self.history_data = []
+
+        self.stop_order_count = 0
+        self.stop_orders = {}
+        self.active_stop_orders = {}
+
+        self.limit_order_count = 0
+        self.limit_orders = {}
+        self.active_limit_orders = {}
+
+        self.trade_count = 0
+        self.trades = {}
+
+        self.logs = []
+
+        self.daily_results = {}
+        self.daily_df = None
+
+        self.path = Path(Path.cwd()/"strategies"/"PaperAccount_reord_table.xlsx")
+        self.trade_record_dict = {}
+
+        self.strategy_name = real_strategy_name
+     
+        self.pnl_list = []
+        self.total_pnl = 0
+
+    def set_parameters(
+        self,
+        vt_symbol: str,
+        interval: Interval,
+        start: datetime,
+        rate: float,
+        slippage: float,
+        size: float,
+        pricetick: float,
+        capital: int = 0,
+        end: datetime = None,
+        annual_days: int = 240
+    ):
+        """"""
+        
+        self.vt_symbol = vt_symbol
+        self.rate = rate
+        self.slippage = slippage
+        self.size = size
+        self.start = start
+
+        self.symbol, exchange_str = self.vt_symbol.split(".")
+        self.exchange = Exchange(exchange_str)
+
+        self.capital = capital
+        self.end = end
+        self.annual_days = annual_days
+
+    def get_trade_record(self):
+        """"""
+        self.trade_record_wb = openpyxl.load_workbook(self.path)
+
+        sheet_names = self.trade_record_wb.sheetnames
+        
+        if self.strategy_name not in sheet_names:
+            print(f"sheet:{self.strategy_name} not in trade record")
+            return
+        else:
+            self.trade_record_sheet = self.trade_record_wb[self.strategy_name]
+
+        total_row = self.trade_record_sheet.max_row
+        total_column = self.trade_record_sheet.max_column
+
+        for i in range(2, total_column+1):
+
+            trade_datetime = self.trade_record_sheet.cell(i, 8).value
+            trade_datetime = trade_datetime.split("+")[0]
+
+            trade = TradeData(
+                    symbol=self.trade_record_sheet.cell(i, 1).value,
+                    orderid=self.trade_record_sheet.cell(i, 2).value,
+                    tradeid=self.trade_record_sheet.cell(i, 3).value,
+                    offset=self.trade_record_sheet.cell(i, 4).value,
+                    direction=self.trade_record_sheet.cell(i, 5).value,
+                    price=self.trade_record_sheet.cell(i, 6).value,
+                    volume=self.trade_record_sheet.cell(i, 7).value,
+                    datetime=datetime.strptime(trade_datetime, "%Y-%m-%d %H:%M:%S.%f")
+                )
+
+            self.trades[trade.tradeid] = trade
+
+    def calculate_result(self):
+        """"""
+        # Add trade data into daily reuslt.
+        for trade in self.trades.values():
+            
+            if trade.direction == "Direction.LONG":
+    
+                trade_money = (trade.price + 0.2) * trade.volume * 10 - trade.volume * 0.1
+
+                self.pnl_list.append(-trade_money)
+
+            elif trade.direction == "Direction.SHORT":
+
+                trade_money = (trade.price - 0.2) * trade.volume * 10 - trade.volume * 0.1
+
+                self.pnl_list.append(trade_money)
+
+        print(self.pnl_list)
+ 
+        self.total_pnl = sum(self.pnl_list)
+
+        print(self.total_pnl)
+
+
+pnl = PnlCaculate("papertest1")
+pnl.get_trade_record()
+pnl.calculate_result()
+
+    
---
+++ 9-脚本自动化/2-代码/no_ui/run_paper_no_ui.py
@@ -0,0 +1,138 @@
+import multiprocessing
+import sys
+from time import sleep
+from datetime import datetime, time
+from logging import INFO
+
+from vnpy.event import EventEngine
+from vnpy.trader.setting import SETTINGS
+from vnpy.trader.engine import MainEngine
+from vnpy.app.paper_account import PaperAccountApp
+
+from vnpy.gateway.ctp import CtpGateway
+from vnpy_ctastrategy import CtaStrategyApp
+from vnpy_ctastrategy.base import EVENT_CTA_LOG
+
+
+SETTINGS["log.active"] = True  # 是否记录日志
+SETTINGS["log.level"] = INFO  # 日志的详细程度，INFO是最低程度的，但也会记录大多数的日志
+SETTINGS["log.console"] = True  # 是否将日志在CMD中逐条打出
+
+
+ctp_setting = {
+    "用户名": "167465",
+    "密码": "hun829248",
+    "经纪商代码": "9999",
+    "交易服务器": "180.168.146.187:10202",
+    "行情服务器": "180.168.146.187:10212",
+    "产品名称": "simnow_client_test",
+    "授权编码": "0000000000000000",
+    "产品信息": "simnow_client_test"
+}
+
+
+# Chinese futures market trading period (day/night)
+DAY_START = time(8, 45)
+DAY_END = time(15, 1)
+
+NIGHT_START = time(20, 45)
+NIGHT_END = time(2, 45)
+
+
+def check_trading_period():
+    """"""
+    current_time = datetime.now().time()
+
+    trading = False
+
+    if (
+        (DAY_START <= current_time <= DAY_END)
+        or (current_time >= NIGHT_START)
+        or (current_time <= NIGHT_END)
+    ):
+        trading = True
+
+    return trading
+
+
+def run_child():
+    """
+    Running in the child process.
+    """
+    SETTINGS["log.file"] = True
+
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)  # 主引擎是事件驱动的，因此只有event_engine这一个入参
+    main_engine.add_gateway(CtpGateway)  # 主引擎添加数据接口
+    paper_engine = main_engine.add_app(PaperAccountApp)  # 主引擎添加CtaStrategyApp，即创建了cta_engine
+    main_engine.write_log("主引擎创建成功")  # 上述步骤全部完成即创建了一个用户所需要的的主引擎
+
+    log_engine = main_engine.get_engine("log")
+    event_engine.register(EVENT_CTA_LOG, log_engine.process_log_event)
+    main_engine.write_log("注册日志事件监听")
+
+    main_engine.connect(ctp_setting, "CTP")
+    main_engine.write_log("连接CTP接口")
+
+    sleep(10)
+
+    paper_engine.init_engine()
+
+    main_engine.write_log("CTA引擎初始化完成")
+
+    HNstrategy_name = "终极震荡指标策略"
+
+    cta_engine.init_strategy(HNstrategy_name)
+    sleep(20)   # Leave enough time to complete strategy initialization
+    
+    main_engine.write_log(f"{HNstrategy_name}完成初始化")
+
+    cta_engine.start_strategy(HNstrategy_name)
+
+    main_engine.write_log(f"{HNstrategy_name}已启动")
+
+    current_time = datetime.now().time()
+
+    main_engine.send_email("终极震荡指标策略启动", f"trading started, {current_time}", SETTINGS["email.receiver"])
+
+    while True:
+        sleep(10)
+
+        trading = check_trading_period()
+        if not trading:
+            print("关闭子进程")
+            current_time = datetime.now().time()
+            main_engine.send_email("终极震荡指标策略关闭", f"trading closed, {current_time}", SETTINGS["email.receiver"])
+            main_engine.close()
+            sys.exit(0)
+
+
+def run_parent():
+    """
+    Running in the parent process.
+    """
+    print("启动CTA策略守护父进程")
+
+    child_process = None
+
+    while True:
+        trading = check_trading_period()
+
+        # Start child process in trading period
+        if trading and child_process is None:
+            print("启动子进程")
+            child_process = multiprocessing.Process(target=run_child)
+            child_process.start()
+            print("子进程启动成功")
+
+        # 非记录时间则退出子进程
+        if not trading and child_process is not None:
+            if not child_process.is_alive():
+                child_process = None
+                print("子进程关闭成功")
+
+        sleep(5)
+
+
+if __name__ == "__main__":
+    run_parent()
